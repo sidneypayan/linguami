404107b4dd69536febded87c95c4397286e440d8
sidney <etreailleurs@hotmail.fr>
2023-08-28 07:18:39 +0200

    working on homepage

--- components/homepage/index.js
+++ components/homepage/index.js
@@ -1,32 +1,20 @@
 import useTranslation from 'next-translate/useTranslation'
+import { useState } from 'react'
 import Image from 'next/image'
 import Hero from './Hero'
 import Link from 'next/link'
 import {
-
 	Box,
 	Button,
 	Container,
-	Divider,
 	Grid,
 	styled,
 	Typography,
+	Modal,
 } from '@mui/material'
 
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
 import { Stack } from '@mui/system'
 
-const StyledStack = styled(Stack)(({ theme }) => ({
-	justifyContent: 'space-between',
-	gap: '2rem',
-	[theme.breakpoints.up('md')]: {
-		flexDirection: 'row',
-	},
-	[theme.breakpoints.down('md')]: {
-		justifyContent: 'center',
-		alignItems: 'center',
-	},
-}))
 const StyledGridItem = styled(Grid)({
 	display: 'flex',
 	flexDirection: 'column',
@@ -37,7 +25,9 @@ const StyledGridItem = styled(Grid)({
 })
 
 const Homepage = () => {
-	const { t, lang } = useTranslation('home')
+	const { t } = useTranslation('home')
+	const [open, setOpen] = useState(false)
+	const [modalName, setModalName] = useState('')
 
 	const multimedia = [
 		{
@@ -67,6 +57,9 @@ const Homepage = () => {
 		},
 	]
 
+	const handleOpen = () => setOpen(true)
+	const handleClose = () => setOpen(false)
+
 	return (
 		<>
 			<Hero />
@@ -74,7 +67,9 @@ const Homepage = () => {
 				<Typography variant='h3' align='center'>
 					{t('multimedia')}
 				</Typography>
+
 				<div className='hr-custom'></div>
+
 				<Grid container justifyContent='space-between' m='4rem auto'>
 					{multimedia.map((icon, index) => (
 						<StyledGridItem key={index} item xs={6} sm={4} lg={2}>
@@ -86,6 +81,31 @@ const Homepage = () => {
 						</StyledGridItem>
 					))}
 				</Grid>
+
+				<Modal
+					open={open}
+					onClose={handleClose}
+					aria-labelledby='modal-modal-title'
+					aria-describedby='modal-modal-description'>
+					<Box
+						sx={{
+							position: 'absolute',
+							top: '50%',
+							left: '50%',
+							transform: 'translate(-50%, -50%)',
+							width: '65%',
+							bgcolor: 'background.paper',
+							boxShadow: 24,
+							p: 4,
+							borderRadius: '4px',
+						}}>
+						<video
+							loop
+							autoPlay
+							style={{ width: '100%' }}
+							src='/img/add_translation.mp4'></video>
+					</Box>
+				</Modal>
 
 				<Stack
 					sx={{
@@ -125,20 +145,129 @@ const Homepage = () => {
 						<Typography variant='subtitle' color='primaryGrey' align='center'>
 							{t('translatorsubtitle')}
 						</Typography>
-						<Link href='/signin'>
-							<Button
-								variant='contained'
-								size='large'
-								sx={{
-									width: '200px',
-									display: 'block',
-									margin: '0 auto',
-									bgcolor: 'clrBtn1',
-									marginTop: '1rem',
-								}}>
-								{t('register')}
-							</Button>
-						</Link>
+
+						<Button
+							onClick={() => handleOpen('translator')}
+							variant='contained'
+							size='large'
+							sx={{
+								width: '200px',
+								display: 'block',
+								margin: '0 auto',
+								bgcolor: 'clrBtn1',
+								marginTop: '1rem',
+							}}>
+							{t('show')}
+						</Button>
+					</Stack>
+				</Stack>
+
+				<Stack
+					sx={{
+						width: '1000px',
+						maxWidth: '100%',
+						flexDirection: 'row',
+						justifyContent: 'space-between',
+						boxShadow: '0px 10px 15px -3px rgba(0,0,0,0.1)',
+						padding: '4rem',
+						borderRadius: '3px',
+						gap: '1.5rem',
+						margin: '0 auto',
+						marginTop: '5rem',
+					}}>
+					<Stack gap='1rem'>
+						<Typography variant='h4' align='center'>
+							{t('dictionary')}
+						</Typography>
+						<Typography variant='subtitle' color='primaryGrey' align='center'>
+							{t('giftranslatorsubtitle')}
+						</Typography>
+
+						<Button
+							onClick={() => handleOpen('dictionary')}
+							variant='contained'
+							size='large'
+							sx={{
+								width: '200px',
+								display: 'block',
+								margin: '0 auto',
+								marginTop: '1rem',
+								bgcolor: 'clrBtn1',
+							}}>
+							{t('show')}
+						</Button>
+					</Stack>
+					<Box
+						width='50%'
+						minWidth={175}
+						sx={{
+							display: {
+								xs: 'none',
+								sm: 'block',
+							},
+						}}>
+						<Box
+							component='img'
+							src='/img/dictionary.png'
+							alt='dictionary'
+							width={175}
+							height={175}
+						/>
+					</Box>
+				</Stack>
+
+				<Stack
+					sx={{
+						width: '1000px',
+						maxWidth: '100%',
+						flexDirection: 'row',
+						justifyContent: 'space-between',
+						boxShadow: '0px 10px 15px -3px rgba(0,0,0,0.1)',
+						padding: '4rem',
+						borderRadius: '3px',
+						gap: '1.5rem',
+						margin: '0 auto',
+						marginTop: '5rem',
+					}}>
+					<Box
+						width='50%'
+						minWidth={175}
+						sx={{
+							display: {
+								xs: 'none',
+								sm: 'block',
+							},
+						}}>
+						<Box
+							component='img'
+							src='/img/flashcards.png'
+							alt='flashcards'
+							width={175}
+							height={175}
+						/>
+					</Box>
+
+					<Stack gap='1rem'>
+						<Typography variant='h4' align='center'>
+							{t('flashcards')}
+						</Typography>
+						<Typography variant='subtitle' color='primaryGrey' align='center'>
+							{t('gifflashcardssubtitle')}
+						</Typography>
+
+						<Button
+							onClick={() => handleOpen('flashcards')}
+							variant='contained'
+							size='large'
+							sx={{
+								width: '200px',
+								display: 'block',
+								margin: '0 auto',
+								bgcolor: 'clrBtn1',
+								marginTop: '1rem',
+							}}>
+							{t('show')}
+						</Button>
 					</Stack>
 				</Stack>
 
@@ -162,20 +291,20 @@ const Homepage = () => {
 						<Typography variant='subtitle' color='primaryGrey' align='center'>
 							{t('teachersubtitle')}
 						</Typography>
-						<Link href='/teacher'>
-							<Button
-								variant='contained'
-								size='large'
-								sx={{
-									width: '200px',
-									display: 'block',
-									margin: '0 auto',
-									marginTop: '1rem',
-									bgcolor: 'clrBtn1',
-								}}>
-								{t('start')}
-							</Button>
-						</Link>
+
+						<Button
+							onClick={handleOpen}
+							variant='contained'
+							size='large'
+							sx={{
+								width: '200px',
+								display: 'block',
+								margin: '0 auto',
+								marginTop: '1rem',
+								bgcolor: 'clrBtn1',
+							}}>
+							{t('start')}
+						</Button>
 					</Stack>
 					<Box
 						width='50%'
@@ -193,75 +322,6 @@ const Homepage = () => {
 							width={175}
 							height={175}
 						/>
-					</Box>
-				</Stack>
-				<Typography variant='h3' align='center' mt='10rem'>
-					Nos outils pour votre apprentissage
-				</Typography>
-				{/* <div style={{ margin: '8rem auto' }} className='hr-custom'></div> */}
-				<div className='hr-custom'></div>
-
-
-
-				<Stack sx={{
-					width: '1000px',
-					maxWidth: '100%',
-					flexDirection: 'row',
-					justifyContent: 'space-between',
-					boxShadow: '0px 10px 15px -3px rgba(0,0,0,0.1)',
-					padding: '4rem',
-					borderRadius: '3px',
-					gap: '4rem',
-					margin: '0 auto',
-
-				}}>
-					<Box>
-						<Typography textAlign='center' variant='h5' mb={5} >
-							{t('giftranslatortitle')}
-						</Typography>
-						<Typography textAlign='center'  >
-							{t('giftranslatorsubtitle')}
-						</Typography>
-					</Box>
-					<Box
-						sx={{ borderRadius: '3px' }}
-						margin='0 auto'
-						component='img'
-						maxHeight='250px'
-						maxWidth='350px'
-						alt='comment traduire'
-						src='/img/add_translation.gif'
-					/>
-				</Stack>
-				<Stack sx={{
-					width: '1000px',
-					maxWidth: '100%',
-					flexDirection: 'row',
-					justifyContent: 'space-between',
-					boxShadow: '0px 10px 15px -3px rgba(0,0,0,0.1)',
-					padding: '4rem',
-					borderRadius: '3px',
-					gap: '4rem',
-					margin: '0 auto',
-					marginTop: '5rem',
-				}}>
-
-					<Box
-						sx={{ borderRadius: '3px' }}
-						margin='0 auto'
-						component='img'
-						maxHeight='250px'
-						maxWidth='350px'
-						alt='comment traduire'
-						src='/img/add_translation.gif'
-					/>
-					<Box>
-						<Typography textAlign='center' variant='h5' mb={5} >
-							{t('flashcards')}
-						</Typography>
-						<Typography textAlign='center'  >
-							{t('gifflashcardssubtitle')}
-						</Typography>
 					</Box>
 				</Stack>
 			</Container>
